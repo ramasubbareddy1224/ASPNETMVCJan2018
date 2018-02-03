c6aec0d12571a8a5023cc55232105d6cf71b485c
ramasubba.m <ramasubba.m@cgi.com>
2018-02-03 08:36:37 +0530

    feb03

--- MVCJan2018/MVCJan2018/Controllers/ProductController.cs
+++ MVCJan2018/MVCJan2018/Controllers/ProductController.cs
@@ -18,15 +18,41 @@ namespace MVCJan2018.Controllers
             // string id = RouteData.Values["id"].ToString();
             return "This is Rama with id:" + id;
         }
-        public ActionResult Info()
+        public ActionResult Info(string id)
         {
-            return View("info1");
+            ViewData["Name"] = "Rama";
+            ViewData["Email"] = "rama@gmail.com";
+            ViewData["Date"] = DateTime.Now;
+
+            ViewBag.VBName = "Siva";
+            ViewBag.VBEmail = "siva@gmail.com";
+            ViewBag.VBDate = DateTime.Now;
+
+            if (id == "redirect")
+            {
+                TempData["TDName"] = "Temp Data Name";
+               // return RedirectToAction("Info2");
+                return RedirectToAction("Info2", "Product");
+            }
+
+            return View();
+        }
+        public ActionResult Info2()
+        {
+            string tempVale = TempData["TDName"].ToString();
+            //TempData.Keep();
+            //return RedirectToAction("Content", "Product");
+            return View();
         }
 
         public ActionResult Content()
         {
             return Content("This is content type");
         }
-         
+        public ActionResult RazorView()
+        {
+            return View();
+        }
+
     }
 }
