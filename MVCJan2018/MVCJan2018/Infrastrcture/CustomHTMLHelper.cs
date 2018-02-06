1a084a352a21836f72a159fc40dcc0daade608d0
ramasubba.m <ramasubba.m@cgi.com>
2018-02-06 08:35:44 +0530

    feb 06

---
+++ MVCJan2018/MVCJan2018/Infrastrcture/CustomHTMLHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCJan2018.Infrastrcture
+{
+    public static class CustomHTMLHelper
+    {
+        public static HtmlString LabelWithBold(string content)
+        {
+            string htmlString = String.Format("<label><strong>{0}</strong></label>", content);
+            return new HtmlString(htmlString);
+        }
+        public static HtmlString LabelWithBoldHTML(this HtmlHelper helper, string content)
+        {
+            string htmlString = String.Format("<label><strong>{0}</strong></label>", content);
+            return new HtmlString(htmlString);
+        }
+
+    }
+}
